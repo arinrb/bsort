bb0cdd437161d8f71e97453bc837be015b6eb957
arinrb <arinrb@gmail.com>
2014-07-07 22:59:57 -0700

    interchange sort check up and working

---
+++ doc/html/group__bsort.js
@@ -0,0 +1,12 @@
+var group__bsort =
+[
+    [ "Interchange_sort", "group__interchange__sort.html", "group__interchange__sort" ],
+    [ "Interchange_sort_asc", "group__interchange__sort__asc.html", "group__interchange__sort__asc" ],
+    [ "Interchange_sort_dec", "group__interchange__sort__dec.html", "group__interchange__sort__dec" ],
+    [ "Interchange_sort_gen", "group__interchange__sort__gen.html", "group__interchange__sort__gen" ],
+    [ "Is_sorted", "group__is__sorted.html", "group__is__sorted" ],
+    [ "Is_sorted_asc", "group__is__sorted__asc.html", "group__is__sorted__asc" ],
+    [ "Is_sorted_dec", "group__is__sorted__dec.html", "group__is__sorted__dec" ],
+    [ "Is_sorted_gen", "group__is__sorted__gen.html", null ],
+    [ "Swap", "group__swap.html", "group__swap" ]
+];
