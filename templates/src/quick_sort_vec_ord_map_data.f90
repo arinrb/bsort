d5e8d9765e4f297cf970c08a2df57ce2ff24d5e9
Arin Richard Bratt <arinrb@gmail.com>
2014-07-08 13:39:31 -0700

    Update quick_sort_vec_ord_map_data.f90
    Not tested yet, but this template should work.

--- templates/src/quick_sort_vec_ord_map_data.f90
+++ templates/src/quick_sort_vec_ord_map_data.f90
@@ -15,11 +15,11 @@
 #IF#ORD#gen#!> @param #ORD_VAR#
 #IF#ORD#gen#!> A logical function which describes the ordering used. The function must take two #DATA_FULL# arguments and return true if and only if the first argument must proceed the second argument in the order.
 #IF#ORD#gen#!>
-!> @notes 
+!> @note
 !> This flat quick sort works by maintaning a psudo stack.  As the the subroutine runs vec splices of vec are broken into two groups based on comparison to a pivot chosen based on the average value of the first and last element of of the slice.  The same process is then applied to the larger of the two slices.  After a slice is sorted the next smallest slice of vec is sorted.  
 !> 
 !> Example ignoring the interchange sort     4 3 7 9 1 2
-!> i    pivot    l_bound   u_bound    
+!> i    pivot    lower     upper   
 !> 1    3        1         6                |2 1 7 9 3 4|
 !> 2    5        3         6                 2 1|4 3 9 7]
 !> 3    8        5         6                 2 1 4 3|7 9|
@@ -27,6 +27,9 @@
 !> 1    1        1         2                |1 2|3 4 7 9
 !> 
 !> The alogorithm of the flat quick sort is harder to follow than the more conventional flat quick sort; however, it saves system resoruces.  Also on systems with limitations on stack sizes a flat quick sort allows sorting of larger lists.  The natural limit is the value of the vector levels.  This can cause a failure in some cases.  
+!>
+!> @note
+!> Error will occur if max_interchange_sort_size is less than one.
 !>
 #IF#ORD#gen#!> @warning
 #IF#ORD#gen#!> The argument types and counts of #ORD_VAR# are not checked within this subroutine.
@@ -48,11 +51,11 @@
 
   ! Variables
   integer :: idx
-  integer :: left
-  integer :: right
+  integer :: low
+  integer :: high
   integer :: lower(n)
   integer :: upper(n)
-  integer :: pivot
+  #DATA_FULL# :: pivot
   
   ! Initilize the stack
   idx = 1
@@ -63,47 +66,58 @@
   do while(idx >= 1)
      
      ! Sort the slice of vec between lower(i) and upper(i)
-     left = lower(i)
-     right = upper(i)
-     if (right - left < max_interchange_sort_size) then
+     low = lower(idx)
+     high = upper(idx)
+     if (high - low < max_interchange_sort_size) then
         ! Run interchange sort on small vectors
-        if (left < right) call interchange_sort_vec_#ORD_NAME#_#MAP_NAME#_#DATA_NAME#(vec(left:right)#MAP_ARG##ORD_ARG#)
+        if (low < high) call interchange_sort_vec_#ORD_NAME#_#MAP_NAME#_#DATA_NAME#(vec(low:high)#MAP_ARG##ORD_ARG#)
         idx = idx - 1
      else
         ! Run quick sort on large vectors
-        ! This pivot helps avoid n^2 behavior, expect in expontially growning and shrinking (reverse) sorted lists and lists of indentical elements
-        pivot = (vec(left) + vec(right)) / 2
-        left = left - 1
-        right = right + 1
+        ! This pivot helps avoid n^2 behavior, expect in expontially growning and shrinking (reverse) sorted lists and lists of indentical elements.
+        pivot = (vec(high) + vec(low)) / 2
+        low = low - 1
+        high = high + 1
         do
-           ! This loop finds elements on the wrong side of the pivot and swaps.  If no such elements are found, exit the loop.
-           do
-              left = left + 1
-              if (vec(left) >= pivot) exit
+           ! This loop finds elements on the wrong side of the pivot and swaps until no such elements are left.
+           
+           ! Find the first element below the pivot that must be above the pivot.
+           do low= low,high
+              if (#ORD#vec(low)#pivot#) exit
            end do
-           do
-              right = right - 1
-              if (vec(right) <= pivot) exit
+
+           ! Find the first element above the pivot that must be below the pivot.
+           do high = high,low,-1
+              if (#ORD#pivot#vec(high)#) exit
            end do
-           if (left < right) then
-              call swap(vec(left),vec(right))
-           elseif(left == right) then
-              ! This adjustment avoids over swaping
-              if (left == lower(idx)) then
-                 left = left + 1
-              else
-                 right = right - 1
-              end if
-              exit
+
+           ! Check the vec(low) needs to be above vec(high)
+           if (#ORD#vec(low)#vec(high)#) then
+              !If vec(low) must follow vec(high) than swap their values
+              call swap(vec(low),vec(high))
            else
+              ! If vec(low) does not have to follow (high) than exit 
+              if (low == lower(idx)) then
+                 if (low == lower(idx)) then
+                 low = low + 1
+              else
+                 high = high - 1
               exit
            end if
         end do
         
-        ! Split splice based on pivot
-        upper(idx + 1) = upper(idx)
-        lower(idx + 1) = left
-        upper(idx) = right
+        ! Split splice based on pivot, making sure to throw the smaller splice on the higher end of the stack
+        if (upper(idx) - low < high - lower(idx)) then
+           upper(idx + 1) = upper(idx)
+           lower(idx + 1) = low
+           upper(idx) = high
+           ! lower(idx) is unchanged
+        else
+           upper(idx + 1) = high
+           lower(idx + 1) = lower(idx)
+           !upper(idx) is unchanged
+           lower(idx) = low
+        end if
         idx = idx + 1
      end if
   end do
